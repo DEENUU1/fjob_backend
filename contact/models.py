787d4d4c1ea1d69af753cbdfb8e0b24af60ac0cc
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-11-27 01:37:33 +0100

    Create Contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    subject = models.CharField(max_length=50)
+    message = models.TextField(max_length=500)
+    email = models.EmailField()
+    reviewed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
