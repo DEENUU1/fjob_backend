3f96ad7683c79f2a428367eefdc394d9fd280337
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-11-28 22:46:34 +0100

    Add migration files

--- company/urls.py
+++ company/urls.py
@@ -3,6 +3,8 @@ from .views import (
     CompanyPublicView,
     CompanyUserView,
     UserCheckCompanyView,
+    CompanyOfferView,
+    CompanyOfferListView
 )
 from django.urls import path
 
@@ -10,9 +12,11 @@ from django.urls import path
 router = DefaultRouter()
 router.register("management", CompanyUserView, basename="company_user")
 router.register("", CompanyPublicView, basename="company_public")
+router.register("company", CompanyOfferView, basename="manage_company")
 
 urlpatterns = [
     path("check/", UserCheckCompanyView.as_view(), name="check_user_company"),
+    path("company/offer", CompanyOfferListView.as_view(), name="list_of_campany_offers"),
 ]
 
 urlpatterns += router.urls
