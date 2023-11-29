27151ffdf8a0f9051c44a8de758639b61eec6e54
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-11-29 11:31:53 +0100

    Delete endpoints for admin user

--- contact/urls.py
+++ contact/urls.py
@@ -1,19 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from .views import (
     ContactViewUser,
-    ContactListViewAdmin,
-    ContactViewAdmin,
-
 )
-from django.urls import path
 
 
 router = DefaultRouter()
 router.register("", ContactViewUser, basename="contact_post")
-router.register("", ContactViewAdmin, basename="contact_post")
 
-urlpatterns = [
-    path("all", ContactListViewAdmin.as_view(), name="salary_stats"),
-]
-
-urlpatterns += router.urls
+urlpatterns = router.urls
--- report/urls.py
+++ report/urls.py
@@ -1,19 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from .views import (
     ReportCreateView,
-    ReportViewSetAdmin,
-    ReportListViewAdmin
 )
-from django.urls import path
 
 
 router = DefaultRouter()
 router.register("", ReportCreateView, basename="report_user")
-router.register("dashboard", ReportViewSetAdmin, basename="report_admin")
 
 
-urlpatterns = [
-    path("dashboard/all", ReportListViewAdmin.as_view(), name="report_list")
-]
-
-urlpatterns += router.urls
+urlpatterns = router.urls
