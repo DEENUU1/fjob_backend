56e5db462b9933478db189a8b741f9715000f257
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-11-29 02:48:34 +0100

    Make all new migrations and create OfferCandidate model

--- candidate/admin.py
+++ candidate/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Candidate, OfferCandidate
 
-# Register your models here.
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone']
+    search_fields = ['full_name', 'email', 'phone']
+    list_filter = ['full_name', 'email', 'phone']
+
+
+class OfferCandidateAdmin(admin.ModelAdmin):
+    list_display = ['candidate', 'offer', 'status']
+    search_fields = ['candidate', 'offer', 'status']
+    list_filter = ['candidate', 'offer', 'status']
+    list_editable = ['status']
+
+
+admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(OfferCandidate, OfferCandidateAdmin)
--- candidate/models.py
+++ candidate/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from offer.models import JobOffer
 from users.models import UserAccount
+from location.models import Address
 
 
 class Candidate(models.Model):
@@ -9,10 +10,24 @@ class Candidate(models.Model):
     phone = models.CharField(max_length=20)
     user = models.ForeignKey(UserAccount, on_delete=models.CASCADE, null=True, blank=True)
     offer = models.ForeignKey(JobOffer, on_delete=models.CASCADE)
-    # address = ...
+    address = models.ManyToManyField(Address, blank=True)
     # resume = ...
     message = models.TextField(max_length=5000, null=True, blank=True)
     url = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.full_name
+
+
+class OfferCandidate(models.Model):
+    STATUS = (
+        ("Pending", "Pending"),
+        ("Reviewed", "Reviewed"),
+        ("Rejected", "Rejected"),
+    )
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    offer = models.ForeignKey(JobOffer, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS)
+
+    def __str__(self):
+        return f"{self.candidate} {self.offer} {self.status}"
