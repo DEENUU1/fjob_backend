03e91f359affe387353600e9e06472c1182db01b
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-11-30 14:15:59 +0100

    Set default status as 'pending' for Candidate model

--- candidate/models.py
+++ candidate/models.py
@@ -19,7 +19,7 @@ class Candidate(models.Model):
     resume = models.FileField()
     message = models.TextField(max_length=5000, null=True, blank=True)
     url = models.URLField(null=True, blank=True)
-    status = models.CharField(max_length=20, choices=STATUS)
+    status = models.CharField(max_length=20, choices=STATUS, default="Pending")
 
     def __str__(self):
         return self.full_name
