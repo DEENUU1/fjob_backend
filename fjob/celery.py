57d9734c759aaab61a404622aa1fd24de9933141
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-07 01:04:41 +0100

    Update django to 5.0 and turned off celery beat for a while beacause of dependencies error

--- fjob/celery.py
+++ fjob/celery.py
@@ -8,21 +8,21 @@ app = Celery("fjob")
 app.config_from_object(settings, namespace="CELERY")
 app.autodiscover_tasks()
 
-app.conf.beat_schedule = {
-    "delete-scraped-offers-after-30-days": {
-        "task": "offer.task.delete_scraped_offers_after_30_days",
-        "schedule": 86400,  # Every 24H
-    },
-    "delete-offers-after-year": {
-        "task": "offer.task.delete_offers_after_year",
-        "schedule": 86400,  # Every 24H
-    },
-    "delete-contact-after-year": {
-        "task": "support.task.delete_contact_after_year",
-        "schedule": 86400,  # Every 24H
-    },
-    "delete-report-after-year": {
-        "task": "support.task.delete_report_after_year",
-        "schedule": 86400,  # Every 24H
-    }
-}
+# app.conf.beat_schedule = {
+#     "delete-scraped-offers-after-30-days": {
+#         "task": "offer.task.delete_scraped_offers_after_30_days",
+#         "schedule": 86400,  # Every 24H
+#     },
+#     "delete-offers-after-year": {
+#         "task": "offer.task.delete_offers_after_year",
+#         "schedule": 86400,  # Every 24H
+#     },
+#     "delete-contact-after-year": {
+#         "task": "support.task.delete_contact_after_year",
+#         "schedule": 86400,  # Every 24H
+#     },
+#     "delete-report-after-year": {
+#         "task": "support.task.delete_report_after_year",
+#         "schedule": 86400,  # Every 24H
+#     }
+# }
