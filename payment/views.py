d212a7e7fe37042f31e7dd0b49ab361adf8e5441
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-09 10:20:46 +0100

    Add endpoint to return list of Product objects in paymnet app

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 from rest_framework import status
 import stripe
 from django.conf import settings
@@ -11,8 +12,19 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import HttpResponse
 from company.models import Company
+from .serializers import ProductSerializer
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset
 
 
 class StripeCheckoutSessionView(APIView):
