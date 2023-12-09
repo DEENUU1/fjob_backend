83e2038ba0bd859b338fb8ce2b7752c53b146d76
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-09 15:17:04 +0100

    Bugfix in stats/signals.py for wrong field name

--- stats/signals.py
+++ stats/signals.py
@@ -6,40 +6,40 @@ from .models import Statistics
 @receiver(post_save, sender='users.UserAccount')
 def stats_user_account(sender, instance, created, **kwargs):
     if created:
-        Statistics.objects.create(model="UserAccount")
+        Statistics.objects.create(model_type="UserAccount")
 
 
 @receiver(post_save, sender='candidate.Candidate')
 def stats_candidate(sender, instance, created, **kwargs):
     if created:
-        Statistics.objects.create(model="Candidate")
+        Statistics.objects.create(model_type="Candidate")
 
 
 @receiver(post_save, sender='company.Company')
 def stats_candidate(sender, instance, created, **kwargs):
     if created:
-        Statistics.objects.create(model="Company")
+        Statistics.objects.create(model_type="Company")
 
 
 @receiver(post_save, sender='offer.JobOffer')
 def stats_scraped_offer(sender, instance, created, **kwargs):
     if created and instance.is_scraped:
-        Statistics.objects.create(model="JobOffer Scraped")
+        Statistics.objects.create(model_type="JobOffer Scraped")
 
 
 @receiver(post_save, sender='offer.JobOffer')
 def stats_saved_offer(sender, instance, created, **kwargs):
     if created and not instance.is_scraped:
-        Statistics.objects.create(model="JobOffer Company")
+        Statistics.objects.create(model_type="JobOffer Company")
 
 
 @receiver(post_save, sender='support.Contact')
 def stats_contact(sender, instance, created, **kwargs):
     if created:
-        Statistics.objects.create(model="Contact")
+        Statistics.objects.create(model_type="Contact")
 
 
 @receiver(post_save, sender='support.Report')
 def stats_report(sender, instance, created, **kwargs):
     if created:
-        Statistics.objects.create(model="Report")
+        Statistics.objects.create(model_type="Report")
