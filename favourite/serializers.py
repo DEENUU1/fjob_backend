c3b86f7751b74ca60dfd8e786c40e0f8fecd6768
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-15 20:49:56 +0100

    FavouriteSerializer add offer serializer with id and title fields

--- favourite/serializers.py
+++ favourite/serializers.py
@@ -1,8 +1,17 @@
 from .models import Favourite
 from rest_framework.serializers import ModelSerializer
+from offer.models import JobOffer
+
+
+class JobOfferSerializer(ModelSerializer):
+    class Meta:
+        model = JobOffer
+        fields = ["id", "title"]
 
 
 class FavouriteSerializer(ModelSerializer):
+    offer = JobOfferSerializer()
+
     class Meta:
         model = Favourite
-        fields = "__all__"
+        fields = ["id", "user", "offer"]
