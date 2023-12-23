a26f239655ae1354d0ed0426c89546af6ce459e4
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-24 00:13:43 +0100

    Bugfix static url

--- fjob/settings.py
+++ fjob/settings.py
@@ -266,8 +266,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
-STATIC_URL = '/static/'
+if WORKING_MODE == "prod":
+    STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+else:
+    STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
