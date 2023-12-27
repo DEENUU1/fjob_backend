0c9595b0c1d55d155c6f9ccf5e88b3586b378fcd
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-28 00:11:33 +0100

    Add ProductListView to urls

--- payment/urls.py
+++ payment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import StripeCheckoutSessionView, stripe_webhook, SuccessView, CancelView, StripeIntentView
+from .views import StripeCheckoutSessionView, stripe_webhook, SuccessView, CancelView, StripeIntentView, ProductListView
 
 urlpatterns = [
+    path("product/", ProductListView.as_view(), name="new_offer_product_list"),
     path("", StripeCheckoutSessionView.as_view(), name="new_company_checkout"),
     path("success/", SuccessView.as_view(), name="success"),
     path("cancel/", CancelView.as_view(), name="cancel"),
