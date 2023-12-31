6c99edc59f520503b626d374f722b3bf7a823257
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-31 02:42:17 +0100

    imprive supprot application admin dashboard

--- support/admin.py
+++ support/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from .models import Contact, Report
 
-admin.site.register(Contact)
-admin.site.register(Report)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["subject", "email", "reviewed", "created_at"]
+    list_filter = ["reviewed", "created_at"]
+    search_fields = ["subject", "email"]
+    list_editable = ["reviewed"]
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ["user", "offer", "reviewed", "created_at"]
+    list_filter = ["reviewed", "created_at"]
+    search_fields = ["user", "offer"]
+    list_editable = ["reviewed"]
+
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Report, ReportAdmin)
