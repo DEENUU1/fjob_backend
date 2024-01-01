ac02a9e89567e31c262af0809820e459455e4e94
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-01 18:16:09 +0100

    Bugfix create_slug change .title to .name

--- company/signals.py
+++ company/signals.py
@@ -8,8 +8,8 @@ from .models import Company
 def create_slug(sender, instance, created, **kwargs):
     # Create slug for Company object based on name and id
     if created:
-        transformed_title = instance.transform_name(instance.title)
-        instance.slug = f"{instance.id}-{transformed_title}"
+        transformed_name = instance.transform_name(instance.name)
+        instance.slug = f"{instance.id}-{transformed_name}"
         instance.save()
 
 
