f7bd69226238a645ee22978f695b06e82c8c2a4e
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-03 00:26:56 +0100

    Fix S3 bucket for production usage

--- fjob/urls.py
+++ fjob/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
     path('api/support/', include('support.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    # urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
