9ff979645afad816cd0b13a3a3cc15dcceb83839
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-04 22:42:06 +0100

    Register candidate model in Admin dashboard

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Candidate
 
-# Register your models here.
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email", "created_at", "status"]
+    list_filter = ["status"]
+    search_fields = ["first_name", "last_name", "email"]
+
+
+admin.site.register(Candidate, CandidateAdmin)
+
