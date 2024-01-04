1430a904188d49975d0b213b828be15f8da7316e
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-04 22:48:32 +0100

    Create Candidate serializers

---
+++ candidates/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Candidate
+
+
+class CandidateCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Candidate
+        fields = "__all__"
+
+
+class CandidateUserSerializer(ModelSerializer):
+    class Meta:
+        model = Candidate
+        fields = [
+            "id",
+            "created_at",
+            "job_offer",
+            "user",
+            "status",
+        ]
+
+
+class CandidateCompanyListSerializer(ModelSerializer):
+    class Meta:
+        model = Candidate
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "message",
+            "created_at",
+            "updated_at",
+            "company",
+            "job_offer",
+            "status",
+            "cv"
+        ]
+
+
+class CandidateCompanyUpdate(ModelSerializer):
+    class Meta:
+        model = Candidate
+        fields = [
+            "id",
+            "company",
+            "status"
+        ]
