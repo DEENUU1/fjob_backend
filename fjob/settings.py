32a954124538fd8247d79a8c928e65daaf872065
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-27 12:43:46 +0100

    Change database to sqlite because of aws ec2 storage limits

--- fjob/settings.py
+++ fjob/settings.py
@@ -205,12 +205,8 @@ elif WORKING_MODE == "prod":
     # }
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("POSTGRES_DB"),
-            "USER": os.getenv("POSTGRES_USER"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "HOST": os.getenv("POSTGRE_HOST"),
-            "PORT": os.getenv("POSTGRE_PORT"),
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 elif WORKING_MODE == "test":
